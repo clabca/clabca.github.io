c5b0e09d3a94814379e92bd253dcc33732a2932c
Cristian Labra Castro <clabca@gmail.com>
2024-02-24 20:24:25 -0300

    agrego portafolio Desarrollo Web

--- PaginaAves/main.py
+++ PaginaAves/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 from generate_html import generate_html_template
 from export_html import export_to_html
@@ -16,23 +15,4 @@ def main():
     export_to_html(html_content)
 
 if __name__ == "__main__":
-=======
-
-from generate_html import generate_html_template
-from export_html import export_to_html
-from data import get_birds_data
-
-
-def main():
-    # Obtener datos de la API
-    birds_data = get_birds_data()
-
-    # Generar contenido HTML
-    html_content = generate_html_template(birds_data)
-
-    # Exportar a archivo HTML
-    export_to_html(html_content)
-
-if __name__ == "__main__":
->>>>>>> 3c3898057c55ed958fb8dddfd515c02e14bd5948
     main()
